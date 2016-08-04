4b1e4635658c2a82b052a0001474cffdd6c1bd41
xc <xcicada824@gmail.com>
2016-08-03 20:08:37 -0400

    add liveload function

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	compass = require('gulp-compass'),
-	ugligy = require('gulp-ugligy');
+	ugligy = require('gulp-uglify'),
+	connect = require('gulp-connect');
 
 
 var env, 
 	sassSource,
 	cssOutput,
-	jsOutput;
+	jsOutput,
+	htmlSource;
 
 env = process.env.NODE_ENV || 'development';
 sassSource = ['development/sass/'];
 cssOutput = ['production/css'];
 jsOutput = ['production/js'];
+sassStyle = 'expanded';
 
 gulp.task('compass', function(){
 	gulp.src(sassSource)
@@ -22,6 +25,25 @@ gulp.task('compass', function(){
 				style: sassStyle
 			})
 			.on('error', gutil.log))
-		.pipe(gulp.dest('production/css'));
+		.pipe(gulp.dest('production/css'))
+		.pipe(connect.reload());
 });
+
+gulp.task('connect', function(){
+	connect.server({
+		root: './',
+		livereload: true
+	});
+});
+
+gulp.task('html', function(){
+	gulp.src('index.html').pipe(connect.reload());
+});
+
+gulp.task('watch', function(){
+	gulp.watch('development/sass/*.scss', ['compass']);
+	gulp.watch('index.html',['html']);
+});
+
+gulp.task('default', ['html', 'compass', 'connect', 'watch']);
 
